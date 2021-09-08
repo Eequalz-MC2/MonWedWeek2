4264f7e51d04006a7427fbd49dc7a5face8571d7
Eequalz-MC2 <89940280+Eequalz-MC2@users.noreply.github.com>
2021-09-08 14:43:39 -0400

    Example3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
-package com.company; 
+package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var reader = new Scanner(System.in); //Make it read someething, but what is the other red squiddley
+
+        System.out.print("How old are you: ");
+
+        var age = reader.nextFloat();
+        var dogYears = age*7;
+        System.out.println( "Wow you are " + age + " that is " + dogYears + " in dog years");
+
+	// write your code heresol
     }
 }
