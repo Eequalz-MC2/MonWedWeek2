244d7e715ad97f270be4819e17a57c7b7c71e06d
Eequalz-MC2 <89940280+Eequalz-MC2@users.noreply.github.com>
2021-09-08 15:03:31 -0400

    Example3

--- src/com/company/Example3.java
+++ src/com/company/Example3.java
@@ -1,4 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Example3 {
+    public static void main(String[] args){
+        var studentNames = new ArrayList<String>();
+        var reader = new Scanner(System.in);//Created a LIST
+        var userResponse = ""; // Variable for User Response
+        while (!userResponse.equals("done")) {
+            System.out.print("Type in the next student name or 'done' to exit: "); // We gotta promt the user some how
+            userResponse = reader.nextLine();
+            if (userResponse.equals("done")){
+                break;// A while loop and we have to put our conditions inside the parenthesis ! IS IF NOT THE SYMBOL "!" MEANS WE DONT WANT IT As long as the user response is not it will keeo doing this.
+            }
+
+            studentNames.add(userResponse);
+
+        }
+
+        System.out.println("All of your students in the class are below:"); // Only double qoutes work not single qoutes like 'Blahh Blahhh'
+
+
+
+    }
 }
