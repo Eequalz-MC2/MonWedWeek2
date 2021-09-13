e218b46e56f55489e3925b9eff6dc46bdf029f5e
Eequalz-MC2 <89940280+Eequalz-MC2@users.noreply.github.com>
2021-09-13 15:06:49 -0400

    Begin Monday 13th

---
+++ src/LineSplitExample.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class LineSplitExample {
+
+    public static void mai(String[] args) throws IOException {
+        var fileName = "jerseys.txt";
+        var studentJerseys = Paths.get(fileName);
+        var studentLines = Files.readAllLines(studentJerseys);//read everything in that one list of string
+
+
+
+
+
+
+
+    }
+}
